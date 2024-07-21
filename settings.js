376f91e193f43d9fc2591b9a6e442e0a6aac4937
Linkiddrak <114172627+Linkiddrak@users.noreply.github.com>
2024-07-21 15:57:15 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ global.owner = process.env.OWNER_NUMBER  || '22871255034' ;  // ADD YOUR NUMBER 
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=bBlw3aIR#bsa-jgSiHE013mKEfCQ3yLkcGWiPT42gXTeEeff4fZ0' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
-MODERATORS : process.env.MODERATORS === undefined ? "22871255034" : process.env.MODERATORS ,    
+MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
 OWNER_NAME: process.env.OWNER_NAME || 'VAJIRA' ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
